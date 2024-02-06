3f07069c7441089220a2d5d79ef8552c0b089e2e
Christo Kruger <christo.kruger@bbd>
2024-02-06 17:11:42 +0200

    asjdbas

--- tests/database.test.js
+++ tests/database.test.js
@@ -5,7 +5,7 @@ function createPool() {
   return mysql.createPool({
     connectionLimit: 10,
     host: "127.0.0.1",
-    user: "root",
+    user: "user",
     password: "password",
     database: "pipelines",
   });
