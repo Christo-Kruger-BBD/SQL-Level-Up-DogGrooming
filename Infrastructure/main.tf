2cd738b5943fe0cfe7fb1e9cec61c7b8820e3f24
DerrykBBD <derryk.fivaz@bbd.co.za>
2024-02-11 21:45:00 +0200

    progress

--- Infrastructure/main.tf
+++ Infrastructure/main.tf
@@ -1,8 +1,56 @@
-module "s3-bucket" {
-  source  = "terraform-aws-modules/s3-bucket/aws"
-  version = "4.1.0"
+# Create a VPC
+resource "aws_vpc" "example_vpc" {
+  cidr_block = "10.0.0.0/16"
 }
-provider "aws" {
-  region = "eu-west-1"
-  profile = "AdministratorAccess-263883060207"
+
+# Create Subnet 1
+resource "aws_subnet" "subnet1" {
+  vpc_id     = aws_vpc.example_vpc.id
+  cidr_block = "10.0.0.0/17"
 }
+
+# Create Subnet 2
+resource "aws_subnet" "subnet2" {
+  vpc_id     = aws_vpc.example_vpc.id
+  cidr_block = "10.0.128.0/17"
+}
+
+
+
+# Create a security group
+resource "aws_security_group" "example_sg" {
+  name        = "example-sg"
+  description = "Example security group"
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  vpc_id = aws_vpc.example_vpc.id
+}
+
+
+resource "aws_db_instance" "default" {
+  allocated_storage = 10
+  engine = "mysql"
+  instance_class = "db.t3.micro"
+  username = "foo"
+  password = "foobarbaz"
+  skip_final_snapshot = true // required to destroy
+}
+
