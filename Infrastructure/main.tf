8512c95ed56b846d1fc37711ecf1601900f3545f
DerrykBBD <derryk.fivaz@bbd.co.za>
2024-02-12 13:54:07 +0200

    Successful connection to rds instance

--- Infrastructure/main.tf
+++ Infrastructure/main.tf
@@ -15,18 +15,15 @@ resource "aws_subnet" "subnet2" {
   cidr_block = "10.0.128.0/17"
 }
 
-
+provider "aws" {
+  region = "eu-west-1"
+  profile = "AdministratorAccess-263883060207"
+}
 
 # Create a security group
 resource "aws_security_group" "example_sg" {
   name        = "example-sg"
   description = "Example security group"
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 
   ingress {
     from_port   = 3306
@@ -52,5 +49,5 @@ resource "aws_db_instance" "default" {
   username = "foo"
   password = "foobarbaz"
   skip_final_snapshot = true // required to destroy
+  publicly_accessible= true
 }
-
