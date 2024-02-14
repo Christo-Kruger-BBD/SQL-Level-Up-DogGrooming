2aaa95327398a3302ca396aa9aa382a268869dec
Christo Kruger <christo.kruger@bbd>
2024-02-14 16:52:28 +0200

    try secrets niw

--- Infrastructure/backend.tf
+++ Infrastructure/backend.tf
@@ -1,4 +1,5 @@
 terraform {
+  experiments = [provider_sensitive_attrs]
   backend "s3" {
     bucket = "doggroomingbucket"
     key = "dog-grooming/terraform.tfstate"  # Specify the path/key for your state file
