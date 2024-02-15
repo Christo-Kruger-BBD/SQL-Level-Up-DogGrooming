a03e10f125e2de78bd75b6ef5ce7dea40a9e87ad
Christo Kruger <christo.kruger@bbd>
2024-02-15 08:30:41 +0200

    askdnajsdna

--- Infrastructure/backend.tf
+++ Infrastructure/backend.tf
@@ -3,7 +3,5 @@ terraform {
     bucket = "doggroomingbucket"
     key = "dog-grooming/terraform.tfstate"  # Specify the path/key for your state file
     region = "eu-west-1"
-    profile = "AdministratorAccess-263883060207"
-    
   }
 }
