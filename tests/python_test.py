3807f81f58e64e756ac95ff49fcc80f34b472a02
Christo Kruger <christo.kruger@bbd>
2024-02-15 22:13:23 +0200

    removed funcs again

--- tests/python_test.py
+++ tests/python_test.py
@@ -98,7 +98,7 @@ def test_data_in_all_tables(db_connection):
     cursor.close()
 
 @pytest.mark.order(4)
-def test_table_views(test_data_in_all_tables):
+def test_table_views(db_connection):
     # Cursor to execute SQL queries
     cursor = db_connection.cursor()
 
@@ -135,38 +135,38 @@ def test_table_views(test_data_in_all_tables):
     cursor.close()
 
 
-@pytest.mark.order(5)
-def test_table_funcs(db_connection):
-    # Cursor to execute SQL queries
-    cursor = db_connection.cursor()
+# @pytest.mark.order(5)
+# def test_table_funcs(db_connection):
+#     # Cursor to execute SQL queries
+#     cursor = db_connection.cursor()
 
-    current_dir = os.path.dirname(os.path.abspath(__file__))
-    sql_script_path = os.path.join(current_dir, '../database/migrations/V6__TableFunctions.sql')
-    execute_sql_script(conn, sql_script_path)
+#     current_dir = os.path.dirname(os.path.abspath(__file__))
+#     sql_script_path = os.path.join(current_dir, '../database/migrations/V6__TableFunctions.sql')
+#     execute_sql_script(conn, sql_script_path)
 
-    current_dir = os.path.dirname(os.path.abspath(__file__))
-    sql_script_path = os.path.join(current_dir, '../database/migrations/V7__TableFunctions2.sql')
-    execute_sql_script(conn, sql_script_path)
+#     current_dir = os.path.dirname(os.path.abspath(__file__))
+#     sql_script_path = os.path.join(current_dir, '../database/migrations/V7__TableFunctions2.sql')
+#     execute_sql_script(conn, sql_script_path)
     
 
-    funcs = [
-        'calculate_total_payment',
-        'get_appointments_for_customer_function'
-    ]
+#     funcs = [
+#         'calculate_total_payment',
+#         'get_appointments_for_customer_function'
+#     ]
 
-    for func in funcs:
+#     for func in funcs:
 
-    # Execute query to count the number of tables
-        cursor.execute("SELECT * FROM " +  func)
+#     # Execute query to count the number of tables
+#         cursor.execute("SELECT * FROM " +  func)
 
-        # Get the count of tables
-        num_data = cursor.fetchone()[0]
+#         # Get the count of tables
+#         num_data = cursor.fetchone()[0]
 
-        # Check if there are 7 tables
-        assert num_data > 0, f"Expected data in view {func}, but found none"
+#         # Check if there are 7 tables
+#         assert num_data > 0, f"Expected data in view {func}, but found none"
 
-    # Close the cursor
-    cursor.close()
+#     # Close the cursor
+#     cursor.close()
 
 
 if __name__ == "__main__":
